308b837e2f471a97d2a056c597cfbd75dcf83c58
JayScriptt <corneliogatbontonjr21@gmail.com>
2025-11-23 21:38:27 +0800

    USER INPUT

--- STRINGS/app.js
+++ STRINGS/app.js
@@ -1,16 +1,10 @@
-// assigning the variable
-let firstName = "Cornelio";
-let lastName = "Gatbonton";
-let hobby = "Coding";
-// applied on click in the external js file for cleaner look insteand of ui
-let submit = (document.getElementById("submit").onclick = function Submit() {
-  document.getElementById("output").innerHTML =
-    // this is the String concatenation
-    `Hello ` +
-    firstName +
-    " " +
-    lastName +
-    " ! " +
-    // this is the template litteral
-    `<br> I Love ${hobby} !`;
-});
+document.getElementById(`submit`).onclick = function Submit() {
+  // getting the name input in first input
+  let name = document.getElementById(`name`).value;
+  // getting the age value in the 2nd input field
+  let age = document.getElementById(`age`).value;
+  // passing the string the message as string interpolation once the button is clicked
+  document.getElementById(
+    "output"
+  ).innerText = `Hello ${name} You Are ${age} years old!`;
+};
